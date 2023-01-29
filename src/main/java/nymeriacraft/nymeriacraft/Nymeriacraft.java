0612f2109cd455eee9b9b9f2e17198bdb7725522
LaimeAranel <69475341+LaimeAranel@users.noreply.github.com>
2023-01-29 23:32:06 +0100

    schule

--- src/main/java/nymeriacraft/nymeriacraft/Nymeriacraft.java
+++ src/main/java/nymeriacraft/nymeriacraft/Nymeriacraft.java
@@ -27,9 +27,10 @@ import nymeriacraft.nymeriacraft.block.ModBlocks;
 import nymeriacraft.nymeriacraft.entity.ModEntityTypes;
 import nymeriacraft.nymeriacraft.item.ModItems;
 import nymeriacraft.nymeriacraft.villager.ModVillagers;
+import nymeriacraft.nymeriacraft.world.feature.ModConfiguredFeatures;
+import nymeriacraft.nymeriacraft.world.feature.ModPlacedFeatures;
 import software.bernie.geckolib3.GeckoLib;
 import org.slf4j.Logger;
-
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(nymeriacraft.MODID)
@@ -39,35 +40,41 @@ public class nymeriacraft {
     public static final String MODID = "nymeriacraft";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "nymeriacraft" namespace
+    // Create a Deferred Register to hold Blocks which will all be registered under
+    // the "nymeriacraft" namespace
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "nymeriacraft" namespace
+    // Create a Deferred Register to hold Items which will all be registered under
+    // the "nymeriacraft" namespace
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
-    // Creates a new Block with the id "nymeriacraft:example_block", combining the namespace and path
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
-    // Creates a new BlockItem with the id "nymeriacraft:example_block", combining the namespace and path
-    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
+    // Creates a new Block with the id "nymeriacraft:example_block", combining the
+    // namespace and path
+    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
+    // Creates a new BlockItem with the id "nymeriacraft:example_block", combining
+    // the namespace and path
+    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block",
+            () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
 
     public nymeriacraft() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
- ModItems.register(modEventBus);
-ModBlocks.register(modEventBus);
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
         // Register the Deferred Register to the mod event bus so blocks get registered
         BLOCKS.register(modEventBus);
         // Register the Deferred Register to the mod event bus so items get registered
         ITEMS.register(modEventBus);
-       // ModVillagers.register(modEventBus);
-        //ModEntityTypes.register(modEventBus);
-
+        // ModVillagers.register(modEventBus);
+        // ModEntityTypes.register(modEventBus);
+        ModConfiguredFeatures.register(modEventBus);
+        ModPlacedFeatures.register(modEventBus);
         GeckoLib.initialize();
 
         MinecraftForge.EVENT_BUS.register(this);
     }
-    
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         // Some common setup code
@@ -82,7 +89,8 @@ ModBlocks.register(modEventBus);
         LOGGER.info("HELLO from server starting");
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    // You can use EventBusSubscriber to automatically register all static methods
+    // in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ClientModEvents {
 
